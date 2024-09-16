b7b46a27ba6f2d498768786660890fdd5b8c02a9
sonicpanther101 <sonicpanther101@gmail.com>
2024-09-16 21:53:09 +1200

    nix test

--- test/examples/build.nix
+++ test/examples/build.nix
@@ -1,7 +1,22 @@
 { pkgs, nozzle }:
 self: super: {
     example-1 = nozzle.cpp-binary {
-        name = "example-1";
+        name = "thing";
         source = [ ./example-1.cpp ];
+    };
+    example-2 = nozzle.cpp-binary {
+        name = "thing";
+        source = [ ./example-2.cpp ];
+        dependencies = [ pkgs.fmt_9 ];
+    };
+    printer = nozzle.cpp-library {
+        name = "printer";
+        headers = [ ./printer.h ];
+        dependencies = [ pkgs.fmt_9 ];
+    };
+    example-3 = nozzle.cpp-binary {
+        name = "thing";
+        source = [ ./example-3.cpp ];
+        dependencies = [ super.printer ];
     };
 }
--- test/flake.nix
+++ test/flake.nix
@@ -10,18 +10,26 @@
             let
                 pkgs = import nixpkgs { 
                     inherit system; 
-                    overlay = [
-                        (self: super: { nozzle = nozzle-base.lib { inherit pkgs; }; })
+                    overlays = [
+                        (self: super: { 
+                            nozzle = nozzle-base.lib { inherit pkgs; }; 
+                            crow = self.nozzle.pkg-wrapper {
+                                name = "crow";
+                                derivation = pkgs.callPackage ./crow.nix {};
+                                cflags = [ "-DCROW_ENABLE_COMPRESSION" ];
+                            }
+                        })
                     ];
-                    };
+                };
                 examples = pkgs.nozzle.add-subdirectory { path = ./examples; };
             in {
                 devShells = {
-                    default = pkgs.mkShell { buildInputs = (with pkgs;[ clang_15 ]); };
+                    default = pkgs.mkShell { buildInputs = (with pkgs;[  ]); };
                 };
                 packages = {
                     default = pkgs.hello;
-                    example-1 = examples.example-1;
-                };
+                    # example-1 = examples.example-1;
+                    # example-2 = examples.example-2;
+                } // examples;
             });
 }
