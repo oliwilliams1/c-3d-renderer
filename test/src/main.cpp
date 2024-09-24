1a839b5eca7f83f57909bd19ac5b9f0075e56cdc
sonicpanther101 <sonicpanther101@gmail.com>
2024-09-25 10:21:24 +1200

    not resizable

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -19,6 +19,7 @@ int main() {
     // Initialize GLFW
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
