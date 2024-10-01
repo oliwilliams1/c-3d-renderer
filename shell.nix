1da7f1bdc53f8fbd7e00a360fdb343880185025c
sonicpanther101 <sonicpanther101@gmail.com>
2024-10-01 19:47:04 +1300

    reformat

--- shell.nix
+++
@@ -1,5 +0,0 @@
-with import <nixpkgs> {};
-let mcc-env = (callPackage /home/adam/mini_compile_commands {}).wrap stdenv;
-in (mkShell.override {stdenv = mcc-env;}) {
-   buildInputs = [ cmake gtest ];
-}
